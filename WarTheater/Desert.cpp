1b71b5895efa6a5d3d46cc0562ecf25c98876d2d
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2022-10-28 19:06:31 +0200

    Added .cpp files for WarTheater
    Added .cpp files for WarTheater.

---
+++ WarTheater/Desert.cpp
@@ -0,0 +1,26 @@
+#include "Desert.h"
+
+Desert::Desert()
+{
+
+}
+
+Desert::~Desert()
+{
+
+}
+
+void Desert::add(Country *force)
+{
+
+}
+
+void Desert::remove(Country *force)
+{
+
+}
+
+void Desert::warLoop()
+{
+
+}
